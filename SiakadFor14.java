6fbbd677eb39d79886c0a927f4bdc67a149e71c5
MochDedy <mochdedy96@gmail.com>
2025-10-27 20:42:46 +0700

    Modifikasi lulus & tdk lulus

--- SiakadFor14.java
+++ SiakadFor14.java
@@ -4,10 +4,11 @@ public class SiakadFor14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int lulus = 0, tdkLulus = 0;
         double nilai, tertinggi = 0, terendah = 100;
 
         for (int i = 1; i <= 10; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke-" +i + ": ");
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
             if (nilai > tertinggi) {
                 tertinggi = nilai;
@@ -15,10 +16,18 @@ public class SiakadFor14 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tdkLulus++;
+            }
         }
 
-        System.out.println("Nilai tertinggi: " +tertinggi);
-        System.out.println("Nilai terendah: " +terendah);
+        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Banyak mahasiswa yang lulus: " + lulus);
+        System.out.println("Banyak mahasiswa yang tidak lulus: " + tdkLulus);
+
     }
 
 }
